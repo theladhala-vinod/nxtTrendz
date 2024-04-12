9f97c70bffce72eba17a2271be6aaef7fb148919
theladhala-vinod <theladhalavinod@gmail.com>
2024-04-12 08:09:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import LoginForm from "./components/LoginForm";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+
+import "./App.css";
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <Route exact path="/" component={Home} />
+    <Route component={NotFound} />
+  </Switch>
+);
 
 export default App;
